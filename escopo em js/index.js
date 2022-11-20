52224d2ef2906c90f78e5a9b8e146551e4f6f298
Andre <devals2022@gmail.com>
2022-11-20 16:41:15 -0300

    primeiro commit

---
+++ escopo em js/index.js
@@ -0,0 +1,157 @@
+//var let const
+
+// escopo global  escopo local
+
+//const animal = 'cachorro';
+
+//if (true) {
+
+  // let animal = 'gato';
+
+ //   console.log(animal);
+    
+//}
+
+//console.log(animal)
+
+//let nome = 'Andre souza';
+//let idade = '45';
+//let mensagem = 'O meu nome é : ' + nome + ' e eu tenho ' +  idade + ' anos';
+
+//console.log(mensagem)
+//console.log('O meu nome é : ', nome.concat(' e eu tenho ', idade, 'anos.'))
+
+              //  OPERADORES ARTMETICOS 
+
+//console.log('Adição (+):', 2 + 2 );
+
+//console.log('Subtração (-):', 10 - 2 );
+
+//console.log('Multiplicação (*):', 5 * 8 );
+
+//console.log('Divisão (/):', 12 / 2 );
+
+//console.log('Modulo (%):', 10 % 3 );
+
+//let valor = 10;
+//console.log('Incremento (++)', ++valor);
+//console.log(valor);
+
+//console.log('Decremento (--)', --valor);
+//console.log(valor);
+
+//console.log('Exponenciação (**):',  2 ** 3);
+
+         //Operadores lógicos
+
+//let animal = 'gato';
+//let idade = 6;
+
+//console.log(animal === 'gato' || idade === '5')
+
+//console.log(animal === 'gato' &&  idade === 5)
+
+//let num = 10;
+
+//console.log(!num)
+
+          //Operadores de comparação
+
+//console.log(10 > 9)
+
+//console.log(10 >= 10)
+
+//console.log(9 < 10)
+
+//console.log(9 <= 10)
+
+//console.log(15 != 11)
+
+// == comparam apenas os valores 
+// === comparam os valores e os tipos das variaveis que no exemplo abaixo sao diferentes . 
+
+//let nun = 18;
+//let idade = '18';
+
+//if (idade === nun) {
+   // console.log('São Iguais')
+//}else{
+    //console.log('Sao Diferentes')
+//}
+
+     // ESTRUTURA IF
+
+//let nome = 'Cláudio';
+
+//if (nome === 'Andre') {
+
+    //console.log('Olá Andre');
+    
+//}else if(nome === 'Maria'){
+    //console.log('Olá Maria');
+//}else{
+  //  console.log('Olá');
+//}
+
+    // ESTRUTURA DE WHILE
+
+//let contador = 0;
+
+//while (contador <= 10) {
+
+   // console.log(contador);
+   // contador++;
+    
+//}
+
+   //ESTRUTURA DO FOR 
+
+//for (i = 0; i <= 10; i++ ) {
+
+   // console.log(i);
+    
+    
+//}
+
+   //comentários
+
+/*
+   
+   console.log('Alo')
+   
+
+
+   function teste(){
+    console.log('Olá :)')
+   }
+
+   teste();
+
+ 
+
+   //funçoes
+
+function teste(){
+    console.log('Olá :)')
+};
+
+teste();
+
+*/
+
+ function mostraNome(nome){
+    return 'Meu nome é ' + nome;
+}
+
+console.log(mostraNome('Andre luiz'));
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/js.exemplo/script.js
@@ -0,0 +1,6 @@
+
+let nome = 'Andre';
+
+let idade = 18;
+
+const dataDeNacimento ='11/08/1977';
